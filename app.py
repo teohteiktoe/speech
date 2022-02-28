c805bd9a617402cf5e8ce9d287418b7a73296618
Teoh TeikToe <ttt@mensa.org.sg>
2022-02-28 18:59:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ from werkzeug.utils import secure_filename
 import speech_recognition as sr
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "static/"
-
+#app.config['UPLOAD_FOLDER'] = "static/"
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        file.save(app.config['UPLOAD_FOLDER'] + filename)
+        file.save("static/"+filename)
+#        file.save(app.config['UPLOAD_FOLDER'] + filename)
         #file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
-        a = sr.AudioFile(app.config['UPLOAD_FOLDER'] + filename)
+#        a = sr.AudioFile(app.config['UPLOAD_FOLDER'] + filename)
+        a = sr.AudioFile("static/"+filename)
         with a as source:
             a = sr.Recognizer().record(source)
         s = sr.Recognizer().recognize_google(a)
